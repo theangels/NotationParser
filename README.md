bd4db54117c297bd61a710f72e1fb0a72bc3e9c3
AngelZheng <theangels373578008@gmail.com>
2022-06-02 10:43:58 +0800

    Add readme
    upload basic function

--- README.md
+++ README.md
@@ -1,1 +1,123 @@
-"# NotationParser" 
+# NotationParser
+
+A tool to convert digital musical notation to pwm.(Like control buzzer)
+
+## TODO
+
+- [x] Rate control
+  - [x] Pitch
+  - [x] Semitone
+  - [x] Octave
+  - [x] Pause
+  - [x] Key Signature
+  - [x] Pre-raised
+- [ ] Time control
+  - [x] BPM
+  - [x] Time Signature
+  - [x] Note
+  - [ ] Mordent
+  - [ ] Tie
+
+## Sample code
+
+![](assets/Snipaste_2022-06-02_10-42-26.jpg)
+
+``` python
+parser = NotationParser()
+
+parser.setKeySignature('C')
+parser.setPreRaised(0)
+parser.setBPM(120)
+parser.setTimeSignature(4)
+
+print(parser.parseNotation('+3..'))
+```
+
+## Use
+
+### Init
+
+``` python
+parser = NotationParser()
+```
+
+### Set key signature
+
+``` python
+parser.setKeySignature('C')
+```
+
+### Set pre-raised scale
+
+``` python
+parser.setPreRaised(0)
+```
+
+### Set BPM
+
+``` python
+parser.setBPM(120)
+```
+
+### Set time signature
+
+``` python
+parser.setTimeSignature(4)
+```
+
+### Get pwm rate and duration
+
+``` python
+parser.parseNotation('+3..')
+```
+
+## Notation
+
+### Key Signature
+
+* C
+* #C
+* bD
+* D
+* #D
+* bE
+* E
+* F
+* #F
+* bG
+* G
+* #G
+* bA
+* A
+* #A
+* bB
+* B
+
+### Pitch
+
+* 1
+* 2
+* 3
+* 4
+* 5
+* 6
+* 7
+
+### Semitone
+
+* b
+
+### Octave
+
+* +
+* -
+
+### Pause
+
+* 0
+
+### Note
+
+* _
+* .
+* &#42;
---
+++ libs/NotationParser.py
@@ -0,0 +1,106 @@
+import math
+import re
+
+
+class NotationParser:
+    def __init__(self):
+        self.setMajor('C')
+
+        self.bpm = 120
+        self.beatNumber = 4
+        self.preRaised = 0
+
+        self.lastFundamentalToneRate = 0
+
+    def setBPM(self, bpm):
+        self.bpm = bpm
+
+    def setTimeSignature(self, beatNumber):
+        self.beatNumber = beatNumber
+
+    def setPreRaised(self, p):
+        self.preRaised = p
+
+    def setKeySignature(self, major):
+        keySignatureDict = {
+            'C': 262,
+            '#C': 277,
+            'bD': 277,
+            'D': 294,
+            '#D': 311,
+            'bE': 311,
+            'E': 330,
+            'F': 349,
+            '#F': 370,
+            'bG': 370,
+            'G': 392,
+            '#G': 415,
+            'bA': 415,
+            'A': 440,
+            '#A': 466,
+            'bB': 466,
+            'B': 494
+        }
+
+        frequency = []
+
+        for i in range(12):
+            f = keySignatureDict[major] * math.pow(2, i/12)
+            frequency.append(f)
+
+        self.scaleDict = {
+            '0': 0,
+            '1': int(frequency[0]),
+            'b2': int(frequency[1]),
+            '2': int(frequency[2]),
+            'b3': int(frequency[3]),
+            '3': int(frequency[4]),
+            '4': int(frequency[5]),
+            'b5': int(frequency[6]),
+            '5': int(frequency[7]),
+            'b6': int(frequency[8]),
+            '6': int(frequency[9]),
+            'b7': int(frequency[10]),
+            '7': int(frequency[11])
+        }
+
+    def parseNotation(self, monophonic):
+        scaleKey = ''
+
+        if(monophonic.find('b') != -1):
+            scaleKey += 'b'
+
+        digitals = re.search(r'\d+', monophonic)
+        scaleKey += digitals.group(0)
+        riseNumber = monophonic.count('+')
+        dropNumber = monophonic.count('-')
+        extendNumber = monophonic.count('.')
+        reduceNumber = monophonic.count('_')
+        dottedNumber = monophonic.count('*')
+        upWaveNumber = monophonic.count('~')
+
+        realRate = self.scaleDict[scaleKey] * \
+            math.pow(2, riseNumber) * (1 / math.pow(2, dropNumber))
+
+        quarterNote = (60 / self.bpm) * self.beatNumber * (1.0 / 4)
+
+        timeValue = quarterNote * (1 / math.pow(2, reduceNumber))
+
+        for _ in range(dottedNumber):
+            timeValue *= 1.5
+
+        timeValue += quarterNote * extendNumber
+
+        return {
+            'rate': int(realRate),
+            'during': int(timeValue * 1000)
+        }
+
+        # if(upWaveNumber == 0):
+        #     self.play(realRate, timeValue)
+        # else:
+        #     self.play(realRate, timeValue * 3.0 / 8)
+        #     self.play(self.lastFundamentalToneRate, timeValue * 1.0 / 4)
+        #     self.play(realRate, timeValue * 3.0 / 8)
+
+        # self.lastFundamentalToneRate = realRate
